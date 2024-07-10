b53562ee21a825143efb758d7a25a2520890db2e
dikshasarasw <renusaraswat620@gmail.com>
2024-07-10 07:58:26 +0530

    first

--- obj.js
+++ obj.js
@@ -10,18 +10,18 @@ obj.fn();//obj(this point to its object)
 obj.fn2();//window(a global object)(arrow function point to parent of 'obj' i.e. global window)
 //this will point to parent object 
 
-// function x(){
-//     console.log(this);//window
-//     function y(){
-//         console.log(this);//window
-//         function z(){
-//             console.log(this);//window
-//         }
-//         z();
-//     }
-//     y();
-// }
-// x();
+function x(){
+    console.log(this);//window
+    function y(){
+        console.log(this);//window
+        function z(){
+            console.log(this);//window
+        }
+        z();
+    }
+    y();
+}
+x();
 
 const per ={
     name:"Aman",
@@ -32,5 +32,7 @@ const per1={
 function a(){
     console.log(this);
 }
+x();//window 
+x.call (this);
 a.call(per);
 a.call(per1);
